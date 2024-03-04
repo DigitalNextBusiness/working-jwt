6d858c76a326747ca1158ebe9d0b825318c5af4a
thedarkknight197 <michele.vitiellobonaventura@gmail.com>
2024-03-04 20:19:38 +0100

    multi app structure

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const config = new DocumentBuilder()
@@ -15,7 +17,15 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
